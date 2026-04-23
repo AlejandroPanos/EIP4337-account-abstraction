0c9417dbd6b156895f8592e6869115b71d22ffbe
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-23 18:12:22 +0200

    [add] - Send packed user op script

---
+++ script/SendPackedUserOp.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+contract SendPackedUserOp is Script {}
