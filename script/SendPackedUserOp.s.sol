a6b75c40f5c3a3b06284d0638882ef95b37cfcc2
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-23 18:25:28 +0200

    [add] - Packed user op bases

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -7,4 +7,10 @@ import {HelperConfig} from "script/HelperConfig.s.sol";
 import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
-contract SendPackedUserOp is Script {}
+contract SendPackedUserOp is Script {
+    /* Library usage */
+    using MessageHashUtils for bytes32;
+
+    /* State variables */
+    uint256 private constant ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+}
