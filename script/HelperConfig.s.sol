969287bdd8f4e59cfcddaa9d5830bc87b2a0faf5
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-23 17:52:57 +0200

    [add] - HerlperConfig.s.sol bases

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {EntryPoint} from "lib/account-abstraction/contracts/core/EntryPoint.sol";
+
+contract HelperConfig is Script {
+    /* State variables */
+    struct NetworkConfig {
+        address entryPoint;
+        address account;
+    }
+
+    uint256 public constant SEPOLIA_ID = 11155111;
+    uint256 public constant LOCAL_ID = 31337;
+    address public constant BURNER = 0x4E80efD8E18250aCD4B14C8e9F873985c3eD6b41;
+    address public constant ANVIL_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+
+    NetworkConfig public activeNetworkConfig;
+}
