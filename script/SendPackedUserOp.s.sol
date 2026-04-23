87270d42c026077c88a6cbaf5d726ebf4eaf2274
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-23 21:11:47 +0200

    [add] - Unsigned user op internal

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -19,11 +19,31 @@ contract SendPackedUserOp is Script {
         bytes memory callData,
         HelperConfig.NetworkConfig memory config,
         address minimalAccount
-    ) public view returns (PackedUserOperation memory) {}
+    ) public view returns (PackedUserOperation memory) {
+        uint256 nonce = vm.getNonce(minimalAccount);
+        PackedUserOperation memory userOp = _generateUnsignedUserOperation(callData, minimalAccount, nonce);
+    }
 
     function _generateUnsignedUserOperation(bytes memory callData, address sender, uint256 nonce)
         internal
         pure
         returns (PackedUserOperation memory)
-    {}
+    {
+        uint128 verificationGasLimit = 16777216;
+        uint128 callGasLimit = verificationGasLimit;
+        uint128 maxPriorityFeePerGas = 256;
+        uint128 maxFeePerGas = maxPriorityFeePerGas;
+
+        return PackedUserOperation({
+            sender: sender,
+            nonce: nonce,
+            initCode: hex"",
+            callData: callData,
+            accountGasLimits: bytes32(uint256(verificationGasLimit) << 128 | callGasLimit),
+            preVerificationGas: verificationGasLimit,
+            gasFees: bytes32(uint256(maxPriorityFeePerGas) << 128 | maxFeePerGas),
+            paymasterAndData: hex"",
+            signature: hex""
+        });
+    }
 }
