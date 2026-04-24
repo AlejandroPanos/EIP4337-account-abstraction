d004b2aa94d24c28f5176793d52094cda73b6b57
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-24 15:58:59 +0200

    [add] - Imports

--- test/unit/SmartWalletTest.t.sol
+++ test/unit/SmartWalletTest.t.sol
@@ -2,5 +2,12 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
+import {SmartWallet} from "src/SmartWallet.sol";
+import {DeploySmartWallet} from "script/DeploySmartWallet.s.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {SendPackedUserOp, PackedUserOperation, IEntryPoint} from "script/SendPackedUserOp.s.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract SmartWalletTest is Test {}
